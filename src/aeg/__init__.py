8a5e342905d12de9906d039a23266e9c31ac2007
Leo Vasanko <leo@git>
2026-01-04 14:25:39 +0000

    Allow the cipher() function also work without prefix aegis

--- src/aeg/__init__.py
+++ src/aeg/__init__.py
@@ -11,6 +11,8 @@ def cipher(alg: CipherName) -> Cipher:
     name = alg.lower().replace("-", "")
     if name == "aegis128":
         name = "aegis128l"  # AEGIS-128 is dead, the user meant AEGIS-128L
+    if not name.startswith("aegis"):
+        name = "aegis" + name
     if name in CIPHERS.values():
         return importlib.import_module(f".{name}", __package__)  # type: ignore[return-value]
     raise ValueError(f"Unknown algorithm {alg!r}. Valid options: {', '.join(CIPHERS)}")
